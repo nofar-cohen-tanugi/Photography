7f01a9cc3d7c0ea31399270c94a3ee0c284c60cb
nofar-cohen-tanugi <nofarcohen19@gmail.com>
2024-01-22 14:34:33 +0200

    add translation

--- photography/src/components/Header.tsx
+++ photography/src/components/Header.tsx
@@ -2,18 +2,21 @@ import React from 'react';
 import { Toolbar } from 'primereact/toolbar';
 import photoLogo from '../assets/images/photoLogo.png';
 import { Image } from 'primereact/image';
+import { useTranslation } from 'react-i18next';
 
 export const Header = () => {
+  const { t } = useTranslation(['header']);
+
   const startContent = (
     <React.Fragment>
-      <p>Contact</p>
+      <p>{t('contact')}</p>
     </React.Fragment>
   );
 
   const centerContent = (
     <div className='flex'>
-      <p className='mx-2'>Packages</p>
-      <p className='mx-2'>Gallery</p>
+      <p className='mx-2'>{t('packages')}</p>
+      <p className='mx-2'>{t('gallery')}</p>
     </div>
   );
 
--- photography/src/main.tsx
+++ photography/src/main.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import '../public/styles/imdex.scss';
+import './assets/styles/imdex.scss';
+import './i18n.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
