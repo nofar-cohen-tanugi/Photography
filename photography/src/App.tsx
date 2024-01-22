07d1eb9032e51eb33fff6b899f3944b8a8de61b1
nofar-cohen-tanugi <nofarcohen19@gmail.com>
2024-01-22 16:03:57 +0200

    fix header + add translation

--- photography/src/App.tsx
+++ photography/src/App.tsx
@@ -2,18 +2,24 @@ import { HomePage } from './pages/HomePage';
 import { ContactPage } from './pages/ContactPage';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Header } from './components/Header';
+import { PrimeReactProvider } from 'primereact/api';
+import Tailwind from 'primereact/passthrough/tailwind';
+import './assets/styles/imdex.scss';
+import './assets/styles/flag.scss';
 
 const App = () => {
   return (
-    <>
-      <Header />
-      <Router>
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/about' element={<ContactPage />} />
-        </Routes>
-      </Router>
-    </>
+    <PrimeReactProvider value={{ unstyled: true, pt: Tailwind }}>
+      <div className='m-3' dir='rtl'>
+        <Header />
+        <Router>
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/about' element={<ContactPage />} />
+          </Routes>
+        </Router>
+      </div>
+    </PrimeReactProvider>
   );
 };
 
--- photography/src/components/Header.tsx
+++ photography/src/components/Header.tsx
@@ -1,34 +1,39 @@
 import React from 'react';
-import { Toolbar } from 'primereact/toolbar';
 import photoLogo from '../assets/images/photoLogo.png';
 import { Image } from 'primereact/image';
 import { useTranslation } from 'react-i18next';
+import { Menubar } from 'primereact/menubar';
+import { MenuItem } from 'primereact/menuitem';
 
 export const Header = () => {
   const { t } = useTranslation(['header']);
 
   const startContent = (
     <React.Fragment>
-      <p>{t('contact')}</p>
-    </React.Fragment>
-  );
-
-  const centerContent = (
-    <div className='flex'>
-      <p className='mx-2'>{t('packages')}</p>
-      <p className='mx-2'>{t('gallery')}</p>
-    </div>
-  );
-
-  const endContent = (
-    <React.Fragment>
       <Image src={photoLogo} alt='Image' width='40' />
     </React.Fragment>
   );
 
+  const endContent = (
+    <React.Fragment>
+      <p>{t('contact')}</p>
+    </React.Fragment>
+  );
+
+  const items: MenuItem[] = [
+    {
+      label: t('packages'),
+      // icon: 'pi pi-home',
+    },
+    {
+      label: t('gallery'),
+      // icon: 'pi pi-star',
+    },
+  ];
+
   return (
     <div className='card'>
-      <Toolbar start={startContent} center={centerContent} end={endContent} />
+      <Menubar model={items} start={startContent} end={endContent} />
     </div>
   );
 };
