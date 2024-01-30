0a2b70553095a8d725eb63e42e8e817a557bf9cc
nofar-cohen-tanugi <nofarcohen19@gmail.com>
2024-01-30 18:44:43 +0200

    fix

--- photography/src/pages/HomePage.tsx
+++ photography/src/pages/HomePage.tsx
@@ -1,27 +1,33 @@
 import { Image } from 'primereact/image';
 import profile from '../assets/images/shimon.jpg';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import axios from 'axios';
 
-export const HomePage = () => {
-  const [data, setData] = useState([]);
+type BaseResponse<T> = {
+  data: T[];
+  message: string;
+};
 
-  useEffect(() => {
+export const HomePage = () => {
+  const [data, setData] = useState([{}]);
+
+  const getSummary = useCallback(async () => {
     // Define the API endpoint
     const apiUrl = '/api/summary';
 
-    // Use Axios to make a GET request
-    axios
-      .get(apiUrl)
-      .then((response: any) => {
-        // Handle the successful response
-        setData(response.data);
-      })
-      .catch((error: any) => {
-        // Handle errors
-        console.error('Error fetching data:', error);
-      });
-  }, []); // The empty dependency array ensures the effect runs only once, equivalent to componentDidMount
+    try {
+      const res: BaseResponse<object> = await axios.get(apiUrl);
+      setData(res.data);
+      console.log(data);
+    } catch (error: unknown) {
+      // Handle errors
+      console.error('Error fetching data:', error);
+    }
+  }, [data]);
+
+  useEffect(() => {
+    getSummary();
+  }, [getSummary]);
 
   return (
     <div className='card pt-8 px-4 flex justify-center items-center flex-wrap'>
@@ -31,16 +37,7 @@ export const HomePage = () => {
         width='400'
         imageClassName='profile-border'
       />
-      <p className='w-3/6 text-white text-x m-5'>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt quasi
-        quae dicta accusantium exercitationem ad deleniti obcaecati? Voluptatem
-        dolores maxime accusantium mollitia, quo laboriosam? Sapiente, ipsum. Ex
-        ab possimus neque? Ad cupiditate ipsa amet hic nam, iure libero
-        praesentium odio ab velit eligendi placeat quam eos molestiae beatae
-        cumque corrupti? Lorem ipsum, dolor sit amet consectetur adipisicing
-        elit. Deserunt quasi quae dicta accusantium exercitationem ad deleniti
-        obcaecati? Voluptatem
-      </p>
+      <p className='w-3/6 text-white text-x m-5'>{'ffffff'}</p>
     </div>
   );
 };
