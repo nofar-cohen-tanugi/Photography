eddb3286bbb34a21e39c4db758ede9d99937db74
nofar-cohen-tanugi <nofarcohen19@gmail.com>
2024-02-01 10:54:25 +0200

    add route

--- server/index.ts
+++ server/index.ts
@@ -19,10 +19,10 @@
 
 import express from "express";
 const app = express();
-//const summaryRoute = require('../routes/summaryRoute');
+const summaryRoute = require('../routes/summaryRoute');
 
 
-//app.use('/api/summary', summaryRoute);
+app.use('/api/summary', summaryRoute);
 app.get("/", (req, res) => { res.send("Express on Vercel"); });
 const PORT = process.env.PORT || 5000; app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });
 
