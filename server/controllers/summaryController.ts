5b11d4613a77271181394faf9fd6c2a7f4d7104d
nofar-cohen-tanugi <nofarcohen19@gmail.com>
2024-02-01 10:39:56 +0200

    try

--- server/controllers/summaryController.ts
+++ server/controllers/summaryController.ts
@@ -1,19 +1,19 @@
 
 import { Request, Response } from 'express';
-import mongoose from 'mongoose';
+//import mongoose from 'mongoose';
 
-const Summary = new mongoose.Schema({
-    title: String,
-    description: String,
-});
+// const Summary = new mongoose.Schema({
+//     title: String,
+//     description: String,
+// });
 
-const SampleModel = mongoose.model('Sample', Summary);
+//const SampleModel = mongoose.model('Sample', Summary);
 
 export const getSummaryData = async (req: Request, res: Response) => {
     try {
-        const data = await SampleModel.find();
-        debugger
-        res.json(data);
+        // const data = await SampleModel.find();
+        // debugger
+        res.json("success!!!");
     } catch (error) {
         console.error('Error fetching data:', error);
         res.status(500).json({ error: 'Internal Server Error' });
