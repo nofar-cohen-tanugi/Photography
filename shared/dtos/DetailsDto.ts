71d085584f752a63af79714cd6715658506d1365
nofar-cohen-tanugi <nofarcohen19@gmail.com>
2024-02-04 10:05:39 +0200

    try fix initialize

--- shared/dtos/DetailsDto.ts
+++ shared/dtos/DetailsDto.ts
@@ -12,6 +12,8 @@ export class DetailsDto {
     constructor(detailsDto: DetailsDto) {
         const mCopy = JSON.parse(JSON.stringify(detailsDto));
         Object.assign(this, mCopy);
+        this.summary = this.summary || { title: '', description: '' };
+        this.contact = this.contact || { phoneNumber: '', email: '' };
     }
 
 }
