94db44dfb02b5fa611af86a6bd3adcc1e48b30ea
nofar-cohen-tanugi <nofarcohen19@gmail.com>
2024-02-18 16:17:01 +0200

    remove unecessary

--- photography/src/App.tsx
+++ photography/src/App.tsx
@@ -15,12 +15,6 @@ const App: React.FC = () => {
   return (
     <div dir='rtl'>
       <div className='gradient'></div>
-
-      {/* <div className='circle xxlarge shade1'></div>
-      <div className='circle xlarge shade2'></div>
-      <div className='circle large shade3'></div>
-      <div className='circle mediun shade4'></div>
-      <div className='circle small shade5'></div> */}
       <Router>
         <Header />
         <Routes>
