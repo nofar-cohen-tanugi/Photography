9d05b3e68b3e36b20d6bb72687b5e8efad25ffa8
nofar-cohen-tanugi <nofarcohen19@gmail.com>
2024-02-23 11:04:04 +0200

    try

--- photography/src/components/Header.tsx
+++ photography/src/components/Header.tsx
@@ -1,70 +1,64 @@
-// import React from 'react';
-// import photoLogo from '../assets/images/photoLogo.jpeg';
-// import { Image } from 'primereact/image';
+import React from 'react';
+import photoLogo from '../assets/images/photoLogo.jpeg';
+import { Image } from 'primereact/image';
 import { useTranslation } from 'react-i18next';
-// import { Menubar } from 'primereact/menubar';
 import { MenuItem } from 'primereact/menuitem';
-// import { Link } from 'react-router-dom';
-// import SelectLanguage from './SelectLanguage';
+import { Link } from 'react-router-dom';
+import SelectLanguage from './SelectLanguage';
 import { PrimeIcons } from 'primereact/api';
 import { MegaMenu } from 'primereact/megamenu';
 
 export const Header = () => {
   const { t } = useTranslation(['header']);
 
-  // const startContent = (
-  //   <React.Fragment>
-  //     <Link to='/'>
-  //       <Image
-  //         src={photoLogo}
-  //         alt='Logo'
-  //         width='50'
-  //         height='100%'
-  //         style={{ margin: '0.4rem 0.5rem 0 0.5rem', cursor: 'pointer' }}
-  //       />
-  //     </Link>
-  //   </React.Fragment>
-  // );
+  const startContent = (
+    <React.Fragment>
+      <Link to='/'>
+        <Image
+          src={photoLogo}
+          alt='Logo'
+          width='50'
+          height='100%'
+          style={{ margin: '0.4rem 0.5rem 0 0.5rem', cursor: 'pointer' }}
+        />
+      </Link>
+    </React.Fragment>
+  );
 
-  // const endContent = (
-  //   <React.Fragment>
-  //     <div className='flex align-items-center gap-2'>
-  //       <SelectLanguage />
-  //     </div>
-  //   </React.Fragment>
-  // );
+  const endContent = (
+    <React.Fragment>
+      <div className='flex align-items-center gap-2'>
+        <SelectLanguage />
+      </div>
+    </React.Fragment>
+  );
 
   const items: MenuItem[] = [
     {
       label: t('packages'),
-      icon: PrimeIcons.QUESTION,
       url: '/packages',
     },
     {
       label: t('gallery'),
-      icon: PrimeIcons.IMAGES,
       url: '/gallery',
     },
     {
       label: t('contact'),
-      icon: PrimeIcons.ENVELOPE,
       url: '/gallery',
     },
   ];
 
   return (
     <div className='card'>
-      <MegaMenu model={items} breakpoint='960px' />
-
-      {/* <Menubar
-        // className='menu-bar p-0 border-0 border-b-2 border-stone-100 bg-transparent flex items-center'
-        // style={{
-        //   padding: '0.1rem 0.5rem 0.1rem 0.5rem',
-        // }}
-        model={[{ id: '1', label: 'packages', icon: PrimeIcons.ALIGN_CENTER }]}
-        // start={startContent}
-        // end={endContent}
-      /> */}
+      <MegaMenu
+        className='menu-bar p-0 border-0 border-b-2 border-stone-100 bg-transparent flex items-center'
+        style={{
+          padding: '0.1rem 0.5rem 0.1rem 0.5rem',
+        }}
+        model={items}
+        start={startContent}
+        end={endContent}
+      />
     </div>
   );
 };
