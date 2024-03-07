0c54e582ff9cac0208d1919fe8476902e2d3bf62
nofar-cohen-tanugi <nofarcohen19@gmail.com>
2024-03-07 15:08:13 +0200

    add key and fix style

--- photography/src/pages/GalleryPage.tsx
+++ photography/src/pages/GalleryPage.tsx
@@ -18,30 +18,32 @@ export const GalleryPage = () => {
     queryFn: getGallery,
   });
 
-  console.log(data?.data);
-
   return (
     <>
       <h1 className='card flex justify-center mt-4 text-2xl py-2'>
         {t('header:gallery')}
       </h1>
       {!isLoading ? (
-        <div className='py-7 px-10 flex-col'>
-          {data?.data?.map((cat) => {
+        <div className='py-2 px-10 flex-col'>
+          {data?.data?.map((cat, index) => {
             return (
-              <div className='py-2'>
-                <p className='text-xl'>{t(`photographyType:${cat.objects[0].category}`)}</p>
-                {cat.objects.map((item) => {
-                  return (
-                    <Image
-                      src={`https://drive.google.com/thumbnail?id=${item.urlId}&sz=w1000`}
-                      alt='Image'
-                      width='250'
-                      preview
-                      style={{ padding: '0.1rem' }}
-                    />
-                  );
-                })}
+              <div key={index} className='py-2 text-center'>
+                <p className='text-xl py-1'>
+                  {t(`photographyType:${cat.objects[0].category}`)}
+                </p>
+                <div className='flex flex-wrap justify-center items-center'>
+                  {cat.objects.map((item) => {
+                    return (
+                      <Image
+                        src={`https://drive.google.com/thumbnail?id=${item.urlId}&sz=w1000`}
+                        alt='Image'
+                        width='250'
+                        preview
+                        style={{ padding: '0.1rem' }}
+                      />
+                    );
+                  })}
+                </div>
               </div>
             );
           })}
