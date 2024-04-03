697ee5acd7c80ee504b1414820e17cddfa61cade
nofar-cohen-tanugi <nofarcohen19@gmail.com>
2024-04-03 18:57:35 +0300

    remove app

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,26 @@
-import app from './app';
+import express from "express";
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from "cors";
+dotenv.config();
+const app = express();
+const detailsRoute = require('./routes/detailsRoute');
+const packagesRoute = require('./routes/packagesRoute');
+const galleryRoute = require('./routes/GalleryRoute');
+
+mongoose.connect(process.env.MONGODB_URI as string);
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+    console.log('MongoDB database connection established successfully');
+});
+
+app.use(cors());
+app.use('/api/details', detailsRoute);
+app.use('/api/packages', packagesRoute);
+app.use('/api/gallery', galleryRoute);
+
+app.get('/', (req, res) => { res.send("Express on Vercel"); });
 
 const PORT = parseInt(process.env.PORT ?? '80');
 
