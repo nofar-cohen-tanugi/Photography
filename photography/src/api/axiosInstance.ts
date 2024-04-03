7c211e54644571c522cc5691868369c0414e7aeb
nofar-cohen-tanugi <nofarcohen19@gmail.com>
2024-04-03 19:27:29 +0300

    fix server url

--- photography/src/api/axiosInstance.ts
+++ photography/src/api/axiosInstance.ts
@@ -1,8 +1,7 @@
 
 import axios from 'axios';
 
-const prodUrl1 = 'https://photography-server-lu9hcr3hz-shimons-projects-9d14bd15.vercel.app';
-
+const prodUrl1 = 'https://photography-server-nine.vercel.app';
 // const prodUrl2 = 'http://localhost:80';
 
 export const axiosInstance = axios.create({
