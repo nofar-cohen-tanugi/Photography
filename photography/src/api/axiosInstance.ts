643370bc352a4ebde6a32206c258dc3ff7384022
nofar-cohen-tanugi <nofarcohen19@gmail.com>
2024-04-08 17:16:01 +0300

    change url server

--- photography/src/api/axiosInstance.ts
+++ photography/src/api/axiosInstance.ts
@@ -1,9 +1,9 @@
 
 import axios from 'axios';
 
-//const prodUrl1 = 'https://photography-server-nine.vercel.app';
-const prodUrl2 = 'http://localhost:80';
+const prodUrl1 = 'https://photography-server-nine.vercel.app';
+// const prodUrl2 = 'http://localhost:80';
 
 export const axiosInstance = axios.create({
-    baseURL: `${prodUrl2}/api`, // Base URL for your API
+    baseURL: `${prodUrl1}/api`, // Base URL for your API
 });
